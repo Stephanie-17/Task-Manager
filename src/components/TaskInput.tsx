1670a2e0dbd2feff183a29268a15381f0c81f640
Stephanie-17 <Stephanie>
2025-07-10 22:27:29 +0100

    Changed the CTA btn

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -11,15 +11,15 @@ const TaskInput = ({
 }: TaskInputProps) => {
 	return (
 		<div className="flex flex-col ml-12 mt-7 md:ml-40">
-			<h1 className="font-semibold text-3xl">My Tasks</h1>
-
+			<h1 className="font-semibold text-3xl mb-5">My Tasks</h1>
+           
 			<form
 				action=""
-				className="flex flex-col sm:flex-row  items-start  sm:items-center gap-2 ml-1 mt-4"
+				className="flex flex-row  items-start  sm:items-center gap-2 ml-1 mt-4"
 				onSubmit={(e) => handleSubmit(e)}
 			>
 				<input
-					className=" border-1  border-[#cfcdea] w-3/4 sm:w-[420px] rounded-md text-sm text-[#0e0c1d] outline-0 p-2 "
+					className=" border-1  border-[#cfcdea] w-3/4 sm:w-[420px] rounded-md text-sm mb-5 text-[#0e0c1d] outline-0 p-2 "
 					type="text"
 					placeholder="Input Task..."
 					value={inputValue}
@@ -27,7 +27,7 @@ const TaskInput = ({
 				/>
 
 				<button
-					className={`bg-[#1500ff]  p-2 rounded-md text-[#ffffff] ${
+					className={`bg-[#1500ff]  pb-1 px-2 text-center rounded-md text-[#ffffff] font-bold text-2xl ${
 						!inputValue
 							? "opacity-50 cursor-not-allowed bg-gray-600 "
 							: "cursor-pointer"
@@ -35,7 +35,7 @@ const TaskInput = ({
 					type="submit"
 					disabled={!inputValue}
 				>
-					Add Task
+					+
 				</button>
 			</form>
 		</div>
